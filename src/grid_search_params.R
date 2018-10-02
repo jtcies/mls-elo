dc9fdf530c97e475ae829a58e73d7e64212785f4
Joseph Ciesielski <jtcies@gmail.com>
2018-10-02 15:57:44 -0400

    using auc and elo.run function

--- src/grid_search_params.R
+++ src/grid_search_params.R
@@ -1,37 +1,36 @@
 library(here)
+library(pROC)
 
 source(here::here("src/helper_funs.R"))
 
 # params ------------
 
-k_value <- seq(20, 50, by = 10)
+k_value <- seq(10, 70, by = 5)
 
-home_bonus <- seq(0, 30, by = 10)
+home_bonus <- seq(0, 200, by = 10)
 
 carry_value <- seq(.5, .9, by = 0.1)
 
-params <- expand.grid(k_value = k_value, 
-                      home_bonus = home_bonus, 
-                      carry_value = carry_value)
+params <- expand.grid(
+  k_value = k_value, 
+  home_bonus = home_bonus
+  #carry_value = carry_value
+  )
 
 
 # find ideal params -----------
 
-grid_search <- function(k_value, home_bonus, carry_value) {
+grid_search <- function(k_value) {
   
-  # this will take a few minutes to run
-  running_elo <- elo_calc(games, teams, k_value = k_value, 
-                          home_bonus = home_bonus, carry_value = carry_value)
+  dat <- elo.run(score(home_final, away_final) ~ home_team + away_team,
+                 data = games,
+                 k = k_value)
   
-  # fill in missing dates
-  complete_elo <- running_elo %>% 
-    fill_elo() %>% 
-    mutate(season = year(date)) %>% 
-    filter(date <= today())
   
-  accuracy_count(games, complete_elo)
+  pROC::auc(dat)
   
 }
+
 
 # import ---------------
 
@@ -46,14 +45,10 @@ teams <- "data/processed/mls_teams.csv" %>%
 
 # run -----------
 
-param_acc <- pmap(params, grid_search)
+param_acc <- map(k_value, grid_search)
 
-params_final <- bind_cols(params, acc = unlist(param_acc))
+params_final <- bind_cols(params, auc = unlist(param_acc))
 
 # visualize --------------
-
-ggplot(params_final, aes(k_value, acc)) +
-  geom_line() +
-  facet_grid(home_bonus ~ carry_value )
 
 write_csv(params_final, here::here("output/param_grid_search_results.csv"))
